634236d9f8c5ca916821ce01dc5df793859945f0
Noah Friedman <noahfriedman2@gmail.com>
2021-06-23 21:11:32 -0400

    Use Redirect function in server executable

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-import unit "unit.nginx.org/go"
+import (
+	"net/http"
+	unit "unit.nginx.org/go"
+)
 
 func main() {
-	if e := unit.ListenAndServe(":8080", nil); e != nil {
+	if e := unit.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, Redirect(r.URL), http.StatusMovedPermanently)
+	})); e != nil {
 		panic(e)
 	}
 }
