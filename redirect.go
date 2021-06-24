5e646069c7201a984402665685b4b1e85bf67058
Noah Friedman <noahfriedman2@gmail.com>
2021-06-23 21:42:01 -0400

    Force redirect to speely.net

--- redirect.go
+++ redirect.go
@@ -4,7 +4,7 @@ import "net/url"
 
 // Redirect takes an HTTP url and converts it to an HTTPS url.
 func Redirect(url *url.URL) string {
-	r := "https://" + url.Host + url.Path
+	r := "https://speely.net" + url.Path
 
 	if url.RawQuery != "" {
 		r += "?" + url.RawQuery
